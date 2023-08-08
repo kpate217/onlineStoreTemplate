8db4c9c6c07bb30a18a7c60565d3614221b06afb
Michelle <michsobol@gmail.com>
2023-08-08 18:52:23 -0400

    Fixed Tests
    tests work 10/10

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from testing.auth_tests import test_hash_password_generates_salt, test_salt_length, test_hash_password_returns_given_salt, test_hash_password_uses_given_salt
 from core.utils import generate_unique_id
 from datetime import datetime
-from testing.db_tests import test_init_db, test_get_inventory_exists, test_dict_factory_link, test_check_connection_threaded,test_get_breadstick_exists,test_get_soup_exists,test_get_mango_price,test_wrong_mango_price
+from testing.db_tests import test_init_db, test_get_inventory_exists, test_dict_factory_link, test_check_connection_threaded,test_get_breadstick_exists,test_get_soup_exists,test_get_mango_price,test_wrong_mango_price,test_get_steak_exists,test_get_pasta_exists
 from testing.core_tests import test_init_sessions, test_add_new_session, test_get_session
 import os
 
@@ -18,7 +18,9 @@ DB_FUNCS = [test_init_db, test_get_inventory_exists,
             test_get_breadstick_exists,
             test_get_soup_exists,
             test_get_mango_price,
-            test_wrong_mango_price]
+            test_wrong_mango_price,
+            test_get_steak_exists,
+            test_get_pasta_exists]
 
 CORE_FUNCS = [test_init_sessions,
               test_add_new_session,
